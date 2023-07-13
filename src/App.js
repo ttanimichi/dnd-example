3dfb2ec294cb4b4214530b14e05ffd39c31e6854
Tsukuru Tanimichi <info@ttanimichi.com>
2023-07-13 16:55:01 +0900

    Multi draggable items

--- src/App.js
+++ src/App.js
@@ -7,29 +7,53 @@ import { Draggable } from "./Draggable";
 export default function App() {
   const containers = ["A", "B", "C"];
   const [parent, setParent] = useState(null);
+  const [parent2, setParent2] = useState(null);
+
   const draggableMarkup = <Draggable id="draggable">Drag me</Draggable>;
+  const draggableMarkup2 = <Draggable id="draggable2">Drag me 2</Draggable>;
+
+  const Tmp = ({ id }) => {
+    if (parent === id && parent2 === id) {
+      return (
+        <>
+          {draggableMarkup}
+          {draggableMarkup2}
+        </>
+      );
+    } else if (parent === id) {
+      return draggableMarkup;
+    } else if (parent2 === id) {
+      return draggableMarkup2;
+    } else {
+      return `Droppable: ${id}`;
+    }
+  };
 
   return (
     <DndContext onDragEnd={handleDragEnd}>
-      {`parent is ${parent}`}
-
       {parent === null ? draggableMarkup : null}
+      {parent2 === null ? draggableMarkup2 : null}
 
       {containers.map((id) => (
         // We updated the Droppable component so it would accept an `id`
         // prop and pass it to `useDroppable`
         <Droppable key={id} id={id}>
-          {parent === id ? draggableMarkup : "Drop here"}
+          <Tmp id={id} />
         </Droppable>
       ))}
     </DndContext>
   );
 
   function handleDragEnd(event) {
-    const { over } = event;
+    console.log(event);
+    const { over, active } = event;
 
     // If the item is dropped over a container, set it as the parent
     // otherwise reset the parent to `null`
-    setParent(over ? over.id : null);
+    if (active.id === "draggable") {
+      setParent(over ? over.id : null);
+    } else {
+      setParent2(over ? over.id : null);
+    }
   }
 }
