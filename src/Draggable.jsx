1bc970ec5cc7d30ebc59f940a4805972f47364c7
Tsukuru Tanimichi <info@ttanimichi.com>
2023-07-15 07:33:15 +0900

    Change the background color of the dragging card

--- src/Draggable.jsx
+++ src/Draggable.jsx
@@ -2,11 +2,12 @@ import React from 'react';
 import {useDraggable} from '@dnd-kit/core';
 
 export function Draggable(props) {
-  const {attributes, listeners, setNodeRef, transform} = useDraggable({
+  const {isDragging, attributes, listeners, setNodeRef, transform} = useDraggable({
     id: props.id,
   });
   const style = transform ? {
     transform: `translate3d(${transform.x}px, ${transform.y}px, 0)`,
+    backgroundColor: isDragging ? 'lime' : undefined,
   } : undefined;
 
   
