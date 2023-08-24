6b77b7f8bd60866c537414cb5ab56e1bcf0f5bc9
Tsukuru Tanimichi <info@ttanimichi.com>
2023-08-24 16:35:04 +0900

    Add Redo and Undo to AppBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,11 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+import FactoryIcon from "@mui/icons-material/Factory";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import Stack from "@mui/material/Stack";
+import UndoIcon from "@mui/icons-material/Undo";
+import RedoIcon from "@mui/icons-material/Redo";
 
 export default function Header() {
   return (
@@ -14,8 +17,28 @@ export default function Header() {
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             ドラッグ＆ドロップできる組織図
           </Typography>
-          <Button color="inherit">初期状態にリセット</Button>
-          <Button color="inherit">買い物かご</Button>
+          <Stack spacing={2} direction="row">
+            <Button color="inherit" variant="outlined" startIcon={<UndoIcon />}>
+              戻る
+            </Button>
+            <Button color="inherit" variant="outlined" startIcon={<RedoIcon />}>
+              進む
+            </Button>
+            <Button
+              color="inherit"
+              variant="outlined"
+              startIcon={<FactoryIcon />}
+            >
+              初期状態にリセット
+            </Button>
+            <Button
+              color="inherit"
+              variant="outlined"
+              startIcon={<ShoppingCartIcon />}
+            >
+              買い物かご
+            </Button>
+          </Stack>
         </Toolbar>
       </AppBar>
     </Box>
