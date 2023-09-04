ae1f26859462ca2c6f3a8880bbf328d624f68050
Tsukuru Tanimichi <info@ttanimichi.com>
2023-09-04 18:58:33 +0900

    Add a comment

--- src/utils/useDepartments.jsx
+++ src/utils/useDepartments.jsx
@@ -3,6 +3,8 @@ import defaultDepartments from "./defaultDepartments";
 
 // TODO: superjson を導入して Date 等の型にも対応する
 // ref. https://github.com/blitz-js/superjson
+//
+// NOTE: 履歴をスタックで管理するなら localStorage ではなく IndexedDB を使った方が良いかも
 export default function useDepartments() {
   const [departments, setDepartments] = useState(getDepartments());
 
